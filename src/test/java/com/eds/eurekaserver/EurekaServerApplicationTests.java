503eb4de3298ed75567d1d85e962e430abefc384
mahedhar03 <mahendhar0301@gmail.com>
2020-12-13 20:11:34 +0530

    Initial version

---
+++ src/test/java/com/eds/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eds.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
